ff12f7b39b77d3a91a6ee9b97a678a5476444bb7
Iqmal Riffai <m.iqmal.riffai@gmail.com>
2025-12-07 22:53:06 +0700

    feat: categpries

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+export function formatDate(dateString: string): string {
+  return new Date(dateString).toLocaleDateString();
+}
+
+export function formatDateTime(dateString: string): string {
+  return new Date(dateString).toLocaleString();
+}
