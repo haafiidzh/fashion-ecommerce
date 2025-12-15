0f4b2a2546973891ac856a2272b8032d124e6eb3
haafiidzh <hafid.kusuma45@gmail.com>
2025-12-16 00:03:13 +0700

    refactor: change created_at and updated_at types in Role interface from Date to string

--- features/roles/types/role-types.ts
+++ features/roles/types/role-types.ts
@@ -2,8 +2,8 @@ export interface Role {
     id: number;
     name: string;
     guard: string;
-    created_at: Date;
-    updated_at: Date;
+    created_at: string;
+    updated_at: string;
 }
   
 export interface RoleState {
