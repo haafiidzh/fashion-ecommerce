ac1c09be698428ef4bc20e9c88b81aa6f3b29ac6
haafiidzh <hafid.kusuma45@gmail.com>
2025-12-17 21:42:41 +0700

    fix: provider layout on product page

--- components/pages/products-pages.tsx
+++ components/pages/products-pages.tsx
@@ -1,15 +1,12 @@
-'use client';
+"use client";
 
-import React from 'react';
-import ProductList from '@/features/products/components/product-list';
-import { ProductProvider } from '@/features/products/context/product-context';
+import React from "react";
+import ProductList from "@/features/products/components/product-list";
 
 export default function ProductsPages() {
-    return (
-        <ProductProvider>
-            <div className="container mx-auto px-4 py-8">
-                <ProductList />
-            </div>
-        </ProductProvider>
-    );
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <ProductList />
+    </div>
+  );
 }
